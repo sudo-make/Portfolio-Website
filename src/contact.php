64cbc2d00415afd853b1a2193ef5433c39420a39
Callum-James Smith <techdragon@tuta.io>
2020-02-23 13:27:36 +0000

    started on contact system and added projects stuff

---
+++ src/contact.php
@@ -0,0 +1,56 @@
+<?php
+if($_POST)
+{
+	$to_email   	= "csmith99@protonmail.com"; //Recipient email, Replace with own email here
+	$from_email     = 'noreply@sudo-make.co.uk'; //from mail, it is mandatory with some hosts and without it mail might endup in spam.
+	
+	//check if its an ajax request, exit if not
+    if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+		
+		$output = json_encode(array( //create JSON data
+			'type'=>'error', 
+			'text' => 'Sorry Request must be Ajax POST'
+		));
+		die($output); //exit script outputting json data
+    } 
+	
+	//Sanitize input data using PHP filter_var().
+	$user_email		= filter_var($_POST["from-email"], FILTER_SANITIZE_EMAIL);
+	$subject		= filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
+	$message		= filter_var($_POST["msg"], FILTER_SANITIZE_STRING);
+	
+	//additional php validation
+	if(!filter_var($user_email, FILTER_VALIDATE_EMAIL)){ //email validation
+		$output = json_encode(array('type'=>'error', 'text' => 'Please enter a valid email!'));
+		die($output);
+	}
+	if(strlen($subject)<3){ //check emtpy subject
+		$output = json_encode(array('type'=>'error', 'text' => 'Subject is required'));
+		die($output);
+	}
+	if(strlen($message)<3){ //check emtpy message
+		$output = json_encode(array('type'=>'error', 'text' => 'Too short message! Please enter something.'));
+		die($output);
+	}
+	
+	//email body
+	$message_body = $message."\r\n\r\n-"."\r\nEmail : ".$user_email . "This is a test email.";
+	
+	//proceed with PHP email.
+	$headers = 'From: '. $from_email .'' . "\r\n" .
+	'Reply-To: '.$user_email.'' . "\r\n" .
+	'X-Mailer: PHP/' . phpversion();
+	
+	$send_mail = mail($to_email, $subject, $message_body, $headers);
+	
+	if(!$send_mail)
+	{
+		//If mail couldn't be sent output error. Check your PHP email configuration (if it ever happens)
+		$output = json_encode(array('type'=>'error', 'text' => 'Could not send mail! Please check your PHP mail configuration.'));
+		die($output);
+	}else{
+		$output = json_encode(array('type'=>'message', 'text' => 'Thank you for your email'));
+		die($output);
+	}
+}
+?>
